daec4e9073784418950608674ae599eaad9ae903
Madsun Digital Marketing & Media Agency <madsundigi@gmail.com>
2025-09-22 08:19:12 +0000

    make case studies of home page mobile friendly as its look nearby to muc

--- src/components/sections/home/case-stories.tsx
+++ src/components/sections/home/case-stories.tsx
@@ -28,7 +28,7 @@ export default function CaseStories() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {stories.map((story) => (
             <Link key={story.slug} href={`/case-studies/${story.slug}`} passHref>
               <Card className="glass-morphic card-glow border-primary/20 flex flex-col h-full cursor-pointer group">
