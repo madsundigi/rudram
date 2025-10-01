dc197dd1c718345b25d1e2685574453cb3ddd49f
Madsun Digital Marketing & Media Agency <madsundigi@gmail.com>
2025-10-01 09:40:22 +0000

    logo is still not updating what things in logo you want as required i wi

--- src/components/sections/home/cloud-partners.tsx
+++ src/components/sections/home/cloud-partners.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -5,7 +6,14 @@ import content from "@/app/content/partners.json";
 import Image from "next/image";
 
 const AwsLogo = () => (
-    <Image src="/images/aws.jpg" alt="AWS Logo" width={80} height={48} className="h-12 w-auto" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="33" viewBox="0 0 55 33" fill="none" className="h-12 w-auto">
+        <path d="M7.34766 22.5195C7.34766 22.5195 9.17969 22.0996 10.0195 21.0547C10.8594 20.0098 10.5078 17.5195 10.5078 17.5195L8.90039 12.334L6.09375 14.502L7.34766 22.5195Z" fill="#FF9900"/>
+        <path d="M22.8281 3.33984L14.7305 17.5195H22.0996L23.8516 12.334L22.8281 3.33984Z" fill="#232F3E"/>
+        <path d="M30.6523 17.5195H38.0215L31.6758 3.33984L30.6523 17.5195Z" fill="#232F3E"/>
+        <path d="M43.9023 22.5195C43.9023 22.5195 42.0703 22.0996 41.2305 21.0547C40.3906 20.0098 40.7422 17.5195 40.7422 17.5195L42.3496 12.334L45.1562 14.502L43.9023 22.5195Z" fill="#FF9900"/>
+        <path d="M22.0998 17.5195L14.7307 17.5195L19.3811 26.6895C19.3811 26.6895 20.2209 28.3789 22.0998 28.3789C23.9788 28.3789 24.5901 26.6895 24.5901 26.6895L22.0998 17.5195Z" fill="#FF9900"/>
+        <path d="M38.0215 17.5195H30.6523L27.9102 26.6895C27.9102 26.6895 28.75 28.3789 30.6289 28.3789C32.5078 28.3789 33.1191 26.6895 33.1191 26.6895L38.0215 17.5195Z" fill="#FF9900"/>
+    </svg>
 );
 
 const MicrosoftLogo = () => (
@@ -20,7 +28,6 @@ const MicrosoftLogo = () => (
 const XeroLogo = () => (
     <Image src="/images/xero.png" alt="Xero Logo" width={80} height={40} className="h-10 w-auto" />
 );
-
 
 const logos: { [key: string]: React.ReactNode } = {
   aws: <AwsLogo />,
@@ -62,3 +69,4 @@ export default function CloudPartners() {
     </section>
   );
 }
+
